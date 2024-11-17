fe67c712c24d98d8642380c7cce379d32d3ef223
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-11-17 23:04:35 +0530

    scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import PokemonApp from "./Machine Coding/React/Pokemon/PokemonApp";
+import InfinetScrollApp from "./Machine Coding/React/Infinte Scroll Good/InfinetScrollApp";
 function App() {
   return (
     <div>
-      <PokemonApp />
+      <InfinetScrollApp />
     </div>
   );
 }
